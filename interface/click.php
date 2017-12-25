055e4acf8db9d3df25f82363f78f03d4515b9e39
Andrew H <FlamesRunner@users.noreply.github.com>
2017-12-24 19:14:26 -0500

    Update click.php

--- interface/click.php
+++ interface/click.php
@@ -1,57 +1,43 @@
 <?php 
 include 'config.php'; 
 if ($show_counter == "no"){
- die("Counter disabled.");
+    die("Counter disabled.");
 }
 ?>
 
 <!DOCTYPE html>
-
 <html>
-
-<head>
-
-<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-<link href='https://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
-<style type="text/css">
-body {
-font-family: 'Lato', sans-serif;
-}
-</style>
-
-<title><?php echo $page_title; ?></title>
-
-</head>
-<body>
-
-<?php
-$shortcode = $_GET['id'];
-if (!is_numeric($shortcode)){
-die("<br /><center><h3>An invalid short code was supplied.</h3></center>");
-}
-
-if (empty($shortcode)){
-die("<br /><center><h3>Please enter your short code.</h3></center>");
-} else {
-$connection = mysqli_connect($sqlhost, $sqluser, $sqlpass, $sqldb);
-$result = mysqli_query($connection, 'select url, counter from urldata where shortcode="'.$shortcode.'";');
-
-if (mysqli_num_rows($result) == 0){
-die("<br /><center><h3>An invalid short code was supplied.</h3></center>");
-}
-
-$row = mysqli_fetch_array($result, MYSQLI_ASSOC);
-
-$newurl = $row["url"];
-$counter = $row['counter'];
- 
-echo '<br /><center><h3>Full URL:</h3>';
-echo '<p>'.$newurl.'</p>';
-echo '<p>It has gained '.$counter.' views. Nice job!</p></center>';
-}
-?>
-
-</body>
+    <head>
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+        <link href='https://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
+        <style type="text/css">
+            body {
+            font-family: 'Lato', sans-serif;
+            }
+        </style>
+        <title><?php echo $page_title; ?></title>
+    </head>
+    <body>
+    <?php
+    $shortcode = $_GET['id'];
+    if (!is_numeric($shortcode)) die("<br /><center><h3>An invalid short code was supplied.</h3></center>");
+    if (empty($shortcode)){
+        die("<br /><center><h3>Please enter your short code.</h3></center>");
+    } else {
+        $connection = mysqli_connect($sqlhost, $sqluser, $sqlpass, $sqldb);
+        $result = mysqli_query($connection, 'select url, counter from urldata where shortcode="'.$shortcode.'";');
+        if (mysqli_num_rows($result) == 0){
+            die("<br /><center><h3>An invalid short code was supplied.</h3></center>");
+        }
+        $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+        $newurl = $row["url"];
+        $counter = $row['counter'];
+        echo '<br /><center><h3>Full URL:</h3>';
+        echo '<p>'.$newurl.'</p>';
+        echo '<p>It has gained '.$counter.' views. Nice job!</p></center>';
+    }
+    ?>
+    </body>
 </html>
